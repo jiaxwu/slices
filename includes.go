f7c933327ec39980c1e43859c405fdc6755dd3f1
xhsf <827032783@qq.com>
2021-12-21 22:50:38 +0800

    Add Includes()

---
+++ includes.go
@@ -0,0 +1,11 @@
+package slices
+
+// Includes items in slice
+func Includes[T comparable](slice []T, item T) bool {
+	for _, _item := range slice {
+		if _item == item {
+			return true
+		}
+	}
+	return false
+}
